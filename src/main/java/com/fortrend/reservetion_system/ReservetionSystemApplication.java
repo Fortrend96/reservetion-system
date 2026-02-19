ce5bbd1787a2b04e4495ea02e6a16bd19f0b60a2
Сербин Антон <serbin.anton@csoft.ru>
2026-02-19 17:41:32 +0700

    init commit

---
+++ src/main/java/com/fortrend/reservetion_system/ReservetionSystemApplication.java
@@ -0,0 +1,14 @@
+package com.fortrend.reservetion_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservetionSystemApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ReservetionSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fortrend/reservetion_system/ReservetionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fortrend.reservetion_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservetionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
