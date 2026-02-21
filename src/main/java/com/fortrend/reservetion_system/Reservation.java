c9114c9f7952d60969273533c4ea3c3065f863cf
Fortrend96 <fortrender96@gmail.com>
2026-02-21 14:30:56 +0700

    Created classes Reservation,ReservationController, ReservationService, unum ReservationStatus. Created CRUD operations for Reservation.

---
+++ src/main/java/com/fortrend/reservetion_system/Reservation.java
@@ -0,0 +1,14 @@
+package com.fortrend.reservetion_system;
+
+import java.time.LocalDate;
+
+public record Reservation (
+    Long id,
+    Long userId,
+    Long roomId,
+    LocalDate startDate,
+    LocalDate endDate,
+    ReservationStatus status
+) {
+
+}
---
+++ src/main/java/com/fortrend/reservetion_system/ReservationStatus.java
@@ -0,0 +1,7 @@
+package com.fortrend.reservetion_system;
+
+public enum ReservationStatus {
+    PENDING,
+    APPROVED,
+    CANCELLED
+}
