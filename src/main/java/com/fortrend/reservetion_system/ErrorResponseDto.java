18c6e8cc193e012bc84db903e52161c724586b5f
Fortrend96 <fortrender96@gmail.com>
2026-03-03 17:38:56 +0700

    Added classes to handle exceptions.

---
+++ src/main/java/com/fortrend/reservetion_system/ErrorResponseDto.java
@@ -0,0 +1,10 @@
+package com.fortrend.reservetion_system;
+
+import java.time.LocalDateTime;
+
+public record ErrorResponseDto(
+        String message,
+        String detailedMessage,
+        LocalDateTime errorTime
+) {
+}
--- src/main/java/com/fortrend/reservetion_system/ReservationRepository.java
+++ src/main/java/com/fortrend/reservetion_system/ReservationRepository.java
@@ -1,6 +1,46 @@
 package com.fortrend.reservetion_system;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReservationRepository extends JpaRepository<ReservationEntity, Long> {
+//    List<ReservationEntity> findAllByStatusIs(ReservationStatus status);
+
+//    @Query(value = "select * from reservations r where r.status = :status", nativeQuery = true)
+//    List<ReservationEntity> findAllByStatusIs(ReservationStatus status);
+//
+//    @Query("select r from ReservationEntity r where r.roomId = :roomId")
+//    List<ReservationEntity> findAllByRoomId(@Param("roomId") Long roomId);
+//
+//    @Transactional
+//    @Modifying
+//    @Query("""
+//            update ReservationEntity r
+//                set r.userId = :userId,
+//                    r.roomId = :roomId,
+//                    r.startData = :startDate,
+//                    r.endDate = :endDate,
+//                    r.status = :status
+//                                where r.id =:id
+//            """)
+//    int updateAllFields(
+//            @Param("id") Long id,
+//            @Param("userId") Long userId,
+//            @Param("roomID") Long roomID,
+//            @Param("startDate") LocalDate startDate,
+//            @Param("endDate") LocalDate endDate,
+//            @Param("status") ReservationStatus status
+//    );
+
+
+    @Modifying
+    @Query("""
+            update ReservationEntity r 
+               set r.status = :status
+                                where r.id =:id
+            """)
+    void setStatus(@Param("id") Long id,
+                   @Param("status") ReservationStatus status);
 }
